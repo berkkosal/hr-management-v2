05ba14bf7333dada9f5b8b9d71312e9149a9576e
Berk Koşal <kosalnberk@gmail.com>
2023-10-06 21:30:28 +0300

    GlobalTheme oluşturuldu

--- frontend/src/BlogRouter.js
+++ frontend/src/BlogRouter.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-
+import { ThemeProvider } from '@mui/material';
+import theme from './utils/theme/GlobalTheme'
 class BlogRouter extends Component {
 
 
-
     render() {
-
         return (
-            <>
-
-
-                <div className="container">
-                    <Routes>
-                        <Route path='/login' element={<LoginPage/>} />
-                    </Routes>
-                </div>
-
-
-            </>
+            <ThemeProvider theme={theme}>
+                <Routes>
+                    <Route path='/login' element={<LoginPage />} />
+                </Routes>
+            </ThemeProvider>
         )
     }
 }
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,14 +1,15 @@
+
 import React from 'react'
 
 
 
 export default function LoginPage() {
 
+  
 
-    
 
   return (
-    <div>HELLOOOO</div>
+    ""
   )
 
 
