34a855d997cd5d8306dcf8f18844380180ecd78f
Berk Koşal <kosalnberk@gmail.com>
2023-10-06 16:51:25 +0300

    mui dahil edildi

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
+import store from './store'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +11,6 @@ root.render(
 
     <App />
 
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 
 );
